e8d8875d7d2a79eec876a48e30a21c38b359ffee
Colin Dean <git@cad.cx>
2012-05-21 22:20:45 -0400

    add basic views

--- picture_contest.rb
+++ picture_contest.rb
@@ -3,6 +3,30 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require
 
-get '/' do
-  'Picture contest!'
+get '/' do ## show the landing page with an upload form
+  #not much dynamic stuff to do here yet
+  
+  haml :index
+end
+
+post '/submit' do ## accept user submission
+  #check if it's valid
+  
+  #upload picture to S3
+  
+  #store metadata in mongo, including path to image
+  
+  #get the id from mongo
+  
+  #redirect to the pic page
+  redirect to('/pic/%s' % pic_id)
+end
+
+get '/pic/:id' do |id| ## show the picture with share links
+  #load the requested id
+  
+  #get the image path on s3
+  
+  #show the image on the page
+  haml :pic
 end
